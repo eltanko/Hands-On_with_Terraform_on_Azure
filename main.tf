6edd112bf40c609deeef317f5dbf4654de35a21b
darrenheath <dazheath@gmail.com>
2023-11-03 17:00:47 +0000

    adding infra

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+  cloud {
+    organization = "dh-az-terraform"
+
+    workspaces {
+      name = "Hands_On_With_Terraform_On_Azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-7de8faa5-hands-on-with-terraform-on-azure"
+  location = "westus"
+}
+
+module "securestorage" {
+  source               = "app.terraform.io/dh-az-terraform/securestorage/azurerm"
+  version              = "1.0.0"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  environment = "Production"
+  storage_account_name = "dhaztflab8714"
+}
