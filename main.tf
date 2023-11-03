e713c4d45286379d65ab6c08a41506039ca30996
darrenheath <dazheath@gmail.com>
2023-11-03 17:04:01 +0000

    adding formatting

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ module "securestorage" {
   version              = "1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
-  environment = "Production"
+  environment          = "Production"
   storage_account_name = "dhaztflab8714"
 }
